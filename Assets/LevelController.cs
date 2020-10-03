f0fff4e47262d9d6fe3825e370e9e403b3a033b7
Marcus Hammarberg <hammarberg.marcus@gmail.com>
2020-10-03 16:08:12 +0200

    Add initial typewriter script to level controller

--- Assets/LevelController.cs
+++ Assets/LevelController.cs
@@ -5,16 +5,40 @@ using UnityEngine.UI;
 
 public class LevelController : MonoBehaviour
 {
-    public Text storyText;
+    public Text storyTextObject;
+    public Canvas storyCanvas;
+    public float typeWriterDelay = 0.1f;
+    public string storyText = "";
+    private string currentText;
+
     // Start is called before the first frame update
     void Start()
     {
-        storyText.text = "My text";
+        StartCoroutine(ShowText());
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator ShowText()
     {
-        
+        for (int i = 1; i <= storyText.Length; i++)
+        {
+            currentText = storyText.Substring(0, i);
+            storyTextObject.text = currentText;
+
+            yield return new WaitForSeconds(getDelay(i));
+        }
+
+        storyCanvas.enabled = false;
+    }
+
+    private float getDelay(int i)
+    {
+        if (i == storyText.Length)
+        {
+            return 4;
+        }
+        else
+        {
+            return storyText.Substring(i - 1, 1).IndexOfAny(new char[] { '?', '!', '.', ',' }) != -1 ? 1 : typeWriterDelay;
+        }
     }
 }
