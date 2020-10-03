17b0c45241d407a3ce76e834cb549f1dc4ac4b17
Emil Sunesson <sunesson.emil@gmail.com>
2020-10-03 15:52:59 +0200

    Trigger reciver

---
+++ Assets/Scripts/Door.cs
@@ -0,0 +1,27 @@
+﻿using Assets.Scripts;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : Reciver
+{
+    public Sprite activeSprite;
+    public Sprite inActiveSprite;
+
+    private SpriteRenderer sr;
+
+    public void Awake()
+    {
+        sr = GetComponent<SpriteRenderer>();
+    }
+
+    public void Update()
+    {
+        if (IsActive()) {
+            sr.sprite = activeSprite;
+        }
+        else {
+            sr.sprite = inActiveSprite;
+        }
+    }
+}
