4b94ec2adaad1b67a6416d993bf0476aa60ff0e2
Emil Sunesson <sunesson.emil@gmail.com>
2020-10-03 14:21:15 +0200

    Basic level/text controller

---
+++ Assets/LevelController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelController : MonoBehaviour
+{
+    public Text storyText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        storyText.text = "My text";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
