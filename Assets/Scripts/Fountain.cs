29c7a96d689c9d96ad04f0792526fe1ffd5c68c0
Adam Olofsson <bonnadam@hotmail.com>
2020-10-04 14:46:40 +0200

    fountain

---
+++ Assets/Scripts/Fountain.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class Fountain : MonoBehaviour
+{
+    public AudioSource fountainAudio;
+    // Start is called before the first frame update
+    void Start()
+    {
+        fountainAudio.volume = 0.25f;
+        fountainAudio.loop = true;
+        fountainAudio.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
